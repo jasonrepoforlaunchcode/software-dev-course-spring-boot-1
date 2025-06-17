6e392b370ea0f6fce8add9582d6d5e198aae5758
Jbo <jasonrepoforlaunchcode@gmail.com>
2025-06-17 13:52:27 -0500

    Pair Practice with Diviya K, Spring controllers and models

---
+++ src/main/java/com/example/mycollections/controllers/AlbumController.java
@@ -0,0 +1,45 @@
+package com.example.mycollections.controllers;
+
+import com.example.mycollections.models.Album;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/albums")
+public class AlbumController {
+
+    private final List<Album> albums = new ArrayList<>() {{
+        add(new Album("Abbey Road", "The Beatles", 1969, 17));
+        add(new Album("Lemonade", "Beyoncé", 2016, 12));
+        add(new Album("Kind of Blue", "Miles Davis", 1959, 5));
+    }};
+
+    @GetMapping("/json")
+    public List<Album> getAlbumsJson() {
+        return albums;
+    }
+
+    @GetMapping("/html")
+    public String getAlbumsHtml() {
+        String albumList = "<ul>";
+        for (Album album : albums) {
+            albumList += "<li>" + album.getName() + "</li>";
+        }
+        albumList += "</ul>";
+
+        return """
+                <html>
+                    <body>
+                        <h1>Albums</h1>
+                        <ul>
+                """ +
+                albumList +
+                """
+                        </ul>
+                    </body>
+                """;
+    }
+
+}
